4e8429704da2996426e67e5d0dde6525c1c0ceb3
Ana Paula Oliveira de Lima <130050929+ana112358@users.noreply.github.com>
2024-12-08 10:37:05 -0300

    chore: Dependências e base para o projeto SpringBoot

---
+++ src/main/java/com/example/frist_spring_app/FristSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.frist_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/frist_spring_app/FristSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frist_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
