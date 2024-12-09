fa023bcad1f60b167f9bc082a308260455939aab
Ana Paula Oliveira de Lima <130050929+ana112358@users.noreply.github.com>
2024-12-09 15:41:20 -0300

    feat: criar entidade User com campos username, email e password

---
+++ src/main/java/com/example/frist_spring_app/entity/User.java
@@ -0,0 +1,62 @@
+package com.example.frist_spring_app.entity;
+import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_users")
+
+public class User {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userID;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    // Getters e Setters (facultativo, mas recomendado)
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
